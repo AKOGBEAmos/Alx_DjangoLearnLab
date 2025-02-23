ad097f5d1587a88cb8c7595ee84895cad092e2be
sp1der <sp1der@Spydeon>
2025-02-23 21:50:20 +0100

    update on template views

--- django-models/LibraryProject/LibraryProject/settings.py
+++ django-models/LibraryProject/LibraryProject/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'LibraryProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "relationship_app/templates"],
+        'DIRS': [BASE_DIR / "relationship_app/"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
