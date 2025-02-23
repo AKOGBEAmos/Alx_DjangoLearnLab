60e75d809428be40c116e9f39888f411c5a4d4fd
sp1der <sp1der@Spydeon>
2025-02-23 04:22:33 +0100

    list filter update django admin_interface

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -3,6 +3,6 @@ from .models import Book
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')
+    list_filter = ('title', 'author', 'publication_year')
     search_fields = ('title', 'author')
 
