1d93501c462959a957f28cd4558d26141986ba8e
sp1der <sp1der@Spydeon>
2025-03-09 18:04:45 +0100

    Authentication views added

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login
 
 from .models import Book
 from .models import Library
