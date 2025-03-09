8bb48c435e097084791d7dc52cbded4486e5b110
sp1der <sp1der@Spydeon>
2025-03-09 19:44:32 +0100

    Authentication views update

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -13,10 +13,10 @@ urlpatterns = [
     # Class-based view for library details
     path('library/<int:pk>', LibraryDetailView.as_view(), name='library_detail'),
 
-    path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name = 'logout.html'), name='logout'),
-    path('register/', views.register , name='register'),
-    path('profile/', profile, name='profile'),
+    path('login/', LoginView.as_view(template_name="registration/login.html"), name="login"),
+    path('logout/', LogoutView.as_view(template_name ="logout.html"), name="logout"),
+    path('register/', views.register , name="register"),
+    path('profile/', profile, name="profile"),
 
     # Password reset views
     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
