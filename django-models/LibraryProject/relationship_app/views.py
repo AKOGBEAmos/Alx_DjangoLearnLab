fa6baafc2cec5c5ea9ed2fca46d128e408c0796f
sp1der <sp1der@Spydeon>
2025-03-13 15:28:19 +0100

    Authentication views update

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,6 +6,7 @@ from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login
+from django.contrib import messages
 from flask import redirect
 
 from .models import Book
@@ -37,8 +38,15 @@ class SignUpView(CreateView):
     template_name = "relationship_app/register.html"
 
 def register(request):
-    form_class = UserCreationForm
-    success_url = reverse_lazy("login")
+    if request.method == 'POST':
+        form_class = UserCreationForm(request.POST)
+        if form_class.is_valid():
+            form_class.save()
+            messages.success(request, 'Account created successfully! You can now log in.')
+            return redirect('login')  # Redirect to the login view
+    else:
+        form_class = UserCreationForm()
+    
     return render(request, 'relationship_app/register.html', {'form': form_class})
 
 @login_required
